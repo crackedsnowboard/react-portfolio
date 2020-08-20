aa59b7f5e91c696beacd8cb1d13c6432dc387b71
Prince of Kochi <mathenjs@gmail.com>
2020-08-19 23:27:42 -0700

    Updates

---
+++ precache-manifest.d91cd5fa300ecd258843ae2f9540712f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cec106802c5c97f97f1d73c8ad5b831",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "256cc942233cf984a1c8",
+    "url": "/react-portfolio/static/css/main.610471f1.chunk.css"
+  },
+  {
+    "revision": "bcf24d996d986343ad12",
+    "url": "/react-portfolio/static/js/2.d6b06d5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.d6b06d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256cc942233cf984a1c8",
+    "url": "/react-portfolio/static/js/main.d7cbf480.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "1752d0fdc8633207bc94ab65ea3aa781",
+    "url": "/react-portfolio/static/media/headShot.1752d0fd.png"
+  },
+  {
+    "revision": "2bce4a31e7f4338cce040a507de52716",
+    "url": "/react-portfolio/static/media/resume.2bce4a31.png"
+  }
+]);
